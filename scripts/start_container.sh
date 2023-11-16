a1d3b762581bc3d269ae0e5e3ce2df96a44ea867
pawvan1998 <55293135+pawvan1998@users.noreply.github.com>
2023-11-16 19:20:45 +0530

    Add the file for code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,5 +5,5 @@ set -e
 docker pull pawvan1998/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000
+docker run -d -p 5000:5000 pawvan1998/simple-python-flask-app
 
